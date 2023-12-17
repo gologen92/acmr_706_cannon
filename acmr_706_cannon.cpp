8f13a93cd413c6162e9c9acde14e7a95ef27390f
Гена <@GOLOGEN>
2023-12-17 20:22:29 +0300

    Добавьте файлы проекта.

---
+++ acmr_706_cannon.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	string fname = "INPUT.TXT";
+	string oname = "OUTPUT.TXT";
+	double r, x, y;
+	ifstream fileInput(fname);
+	ofstream fileOutput(oname);
+	
+	if (fileInput.is_open())
+	{
+		cout << "Файл открыт" << endl;
+		fileInput >> r >> x >> y;
+	}
+	if (x < 0)
+		x = -x;
+	fileOutput << r * x / (2 * r - y) << endl;
+	fileOutput.close();
+
+}
